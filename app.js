5eb6a1c0026188526fd1b345e95aafe57cc2f4c7
Kaizer-Rai <108696304+Kaizer-Rai@users.noreply.github.com>
2026-08-31 04:23:54 +0000

    added movies up to camp rock 2: the final jam

--- app.js
+++ app.js
@@ -24,8 +24,17 @@ const twilightNM = new Movie('The Twilight Saga: New Moon', 5, 2009, 'Chris Weit
 const twilightE = new Movie('The Twilight Saga: Eclipse', 6, 2010, 'David Slade', ['Drama', 'Fantasy', 'Romance', 'Adventure'], 3, 4, 4, 3, 'None');
 const twilightBD1 = new Movie('The Twilight Saga: Breaking Dawn - Part 1', 7, 2011, 'Bill Condon', ['Drama', 'Fantasy', 'Romance', 'Adventure'], 3, 4, 5, 4, 'None');
 const twilightBD2 = new Movie('The Twilight Saga: Breaking Dawn - Part 2', 8, 2012, 'Bill Condon', ['Drama', 'Fantasy', 'Romance', 'Adventure'], 5, 5, 5, 5, 'None');
+const silenceLambs = new Movie('Silence of the Lambs', 9, 1991, 'Jonathan Demme', ['Crime', 'Thriller', 'Horror', 'Drama'], 5, 5, 4, 3, 'None');
+const nachoLibre = new Movie('Nacho Libre', 10, 2006, 'Jared Hess', ['Family', 'Action', 'Comedy'], 5, 5, 4, 5, 'Lizzie');
+const fantasticFox = new Movie('Fantastic Mr. Fox', 11, 2009, 'Wes Anderson', ['Comedy', 'Animation', 'Family', 'Adventure'], 3, 4, 4, 4, 'None') 
+const donnieDarko = new Movie('Donnie Darko', 12, 2001, 'Richard Kelly', ['Drama', 'Fantasy', 'Mystery'], 4, 4, 4, 4, 'None');
+const kabbieKushi = new Movie('Kabhii Khushi Kabhie Gham', 13, 2001, 'Karan Johar', ['Comedy', 'Drama'], 5, 5, 5, 5, 'None');
+const suzume = new Movie('Suzume', 14, 2022, 'Makoto Shinkai', ['Fantasy', 'Animation', 'Adventure', 'Drama'], 3, 3, 4, 3, 'None')
+const itsBegXmas = new Movie("It's Beginning to Look a Lot Like Christmas", 15, 2019, 'David Weaver', ['Romance', 'TV Movie'], 1, 4, 5, 3, 'None');
+const campRock = new Movie('Camp Rock', 16, 2008, 'Matthew Diamon', ['Family', 'Music', 'TV Movie'], 3, 4, 4, 3, 'None');
+const campRock2 = new Movie('Camp Rock 2: The Final Jam', 17, 2010, 'Paul Hoen', ['Family', 'Music', 'TV Movie'], 3, 3, 3, 4, 'None');
 
-const movies = [lotrF, lotrTT, lotrRotT, twilight, twilightNM, twilightE, twilightBD1, twilightBD2];
+const movies = [lotrF, lotrTT, lotrRotT, twilight, twilightNM, twilightE, twilightBD1, twilightBD2, silenceLambs, nachoLibre, fantasticFox, donnieDarko, kabbieKushi, suzume, itsBegXmas, campRock, campRock2];
 
 function getFilteredAndSorted() {
   const query = document.getElementById('filter-input').value.toLowerCase().trim();
